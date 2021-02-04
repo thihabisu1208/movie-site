fb4c89aa648818169c0c1bb7a2ecf2ae024fe7cd
kiyoharu kikuchi <k-kikuchi@bfp.co.jp>
2021-02-04 19:05:01 +0900

    SPの実装

--- src/js/common/_Navigation.js
+++ src/js/common/_Navigation.js
@@ -2,7 +2,10 @@ export default class Navigation {
   constructor() {
     this.urlParams = window.location.pathname;
     this.$headerNav = $(".l-header_nav");
+    this.$headerSPNav = $(".l-sp_nav");
+    this.$hambergerSP = $(".l-sp_ham");
     this.$headerNavItems = this.$headerNav.find(".l-header_nav-item");
+    this.$headerSpItems = this.$headerSPNav.find(".l-sp_nav-item");
     this.$footerNav = $(".l-footer_nav");
     this.$footerNavItems = this.$footerNav.find(".l-footer_nav-item");
   }
@@ -16,10 +19,18 @@ export default class Navigation {
   addEvents() {
     this.onClickNavHeader();
     this.onClickNavFooter();
+    this.$hambergerSP.on('click', () => this.onClickHumberger());
   }
 
   onClickNavHeader() {
     this.$headerNavItems
+      .find(`a[href$="${this.urlParams}"]`)
+      .closest("li")
+      .addClass("is-active")
+      .siblings("li")
+      .addClass("is-inactive");
+
+      this.$headerSpItems
       .find(`a[href$="${this.urlParams}"]`)
       .closest("li")
       .addClass("is-active")
@@ -34,5 +45,9 @@ export default class Navigation {
       .addClass("is-active")
       .siblings("li")
       .addClass("is-inactive");
+  }
+
+  onClickHumberger() {
+    $("body, .l-sp_ham, .l-header_sp").toggleClass('active');
   }
 }
